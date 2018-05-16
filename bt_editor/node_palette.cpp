d79d5a7698c15a804ef5f01d6a85a201ed2ae423
Davide Faconti <davide.faconti@gmail.com>
2018-05-16 12:29:37 +0200

    WIP: Node Palette

--- bt_editor/node_palette.cpp
+++ bt_editor/node_palette.cpp
@@ -1,12 +1,18 @@
 #include "node_palette.h"
 #include "ui_node_palette.h"
 
+#include <QComboBox>
+#include <QHeaderView>
+#include <QPushButton>
+
 NodePalette::NodePalette(TreeNodeModels &tree_nodes_model, QWidget *parent) :
     QFrame(parent),
     ui(new Ui::NodePalette),
     _tree_nodes_model(tree_nodes_model)
 {
     ui->setupUi(this);   
+    ui->paramsFrame->setHidden(true);
+    ui->buttonsFrame->setHidden(true);
 }
 
 NodePalette::~NodePalette()
@@ -24,17 +30,13 @@ void NodePalette::updateTreeView()
       item->setFont(0, font);
     };
 
-    auto skipText = QStringLiteral("skip me");
-
     ui->treeWidget->clear();
     _tree_view_category_items.clear();
 
     for (const QString& cat : {"Root", "Action", "Decorator", "Control", "SubTree" } )
     {
-      auto item = new QTreeWidgetItem(ui->treeWidget);
-      item->setText(0, cat);
+      auto item = new QTreeWidgetItem(ui->treeWidget, {cat});
       AdjustFont(item, 11, true);
-      item->setData(0, Qt::UserRole, skipText);
       item->setFlags( item->flags() ^ Qt::ItemIsDragEnabled );
       item->setFlags( item->flags() ^ Qt::ItemIsSelectable );
       _tree_view_category_items[ cat ] = item;
@@ -47,8 +49,7 @@ void NodePalette::updateTreeView()
 
       const QString& category = toStr(model.node_type);
       auto parent = _tree_view_category_items[category];
-      auto item = new QTreeWidgetItem(parent);
-      item->setText(0, ID);
+      auto item = new QTreeWidgetItem(parent, {ID});
       AdjustFont(item, 11, false);
       item->setData(0, Qt::UserRole, ID);
     }
@@ -66,8 +67,7 @@ void NodePalette::updateTreeView()
       const QString& category = it.second;
 
       auto parent = _tree_view_category_items[category];
-      auto item = new QTreeWidgetItem(parent);
-      item->setText(0, ID);
+      auto item = new QTreeWidgetItem(parent, {ID});
       AdjustFont(item, 11, false);
       item->setData(0, Qt::UserRole, ID);
     }
@@ -77,6 +77,39 @@ void NodePalette::updateTreeView()
 
 void NodePalette::on_treeWidget_itemSelectionChanged()
 {
+  auto selected_items = ui->treeWidget->selectedItems();
+  if(selected_items.size() == 0)
+  {
+    ui->paramsFrame->setHidden(true);
+  }
+  else {
+    auto selected_item = selected_items.front();
+    QString item_name = selected_item->text(0);
+    ui->paramsFrame->setHidden(false);
+    ui->label->setText( item_name + QString(" Parameters"));
+
+    auto & model = _tree_nodes_model[item_name];
+    int row = 0;
+
+    ui->parametersTableWidget->setRowCount(model.params.size());
+
+    connect( ui->parametersTableWidget,  &QTableWidget::cellChanged, this, &NodePalette::on_parameterChanged);
+
+    for (auto& it: model.params)
+    {
+      ui->parametersTableWidget->setItem(row,0, new QTableWidgetItem(it.first));
+      ui->parametersTableWidget->setItem(row,1, new QTableWidgetItem(toStr(it.second)));
+//      auto combo = new QComboBox();
+//      combo->addItems( {"Int", "Double", "Text"} );
+//      combo->setMaximumWidth(80);
+//      ui->parametersTableWidget->setCellWidget(row, 1, combo);
+      row++;
+    }
+
+    ui->parametersTableWidget->horizontalHeader()->setSectionResizeMode(0, QHeaderView::Stretch);
+    ui->parametersTableWidget->horizontalHeader()->setSectionResizeMode(1, QHeaderView::ResizeToContents);
+
+  }
 
 }
 
@@ -98,4 +131,17 @@ void NodePalette::on_lineEditFilter_textChanged(const QString &text)
       }
     }
   }
+}
+
+void NodePalette::on_parametersTableWidget_itemSelectionChanged()
+{
+  auto selected_items = ui->parametersTableWidget->selectedItems();
+
+  ui->pushButtonDelete->setEnabled( selected_items.size() != 0 );
+}
+
+void NodePalette::on_parameterChanged(int row, int col)
+{
+  ui->pushButtonAdd->setEnabled(true);
+
 }
--- bt_editor/node_palette.h
+++ bt_editor/node_palette.h
@@ -3,6 +3,7 @@
 
 #include <QFrame>
 #include <QTreeWidgetItem>
+#include <QTableWidgetItem>
 #include "XML_utilities.hpp"
 
 namespace Ui {
@@ -24,6 +25,10 @@ private slots:
     void on_treeWidget_itemSelectionChanged();
 
     void on_lineEditFilter_textChanged(const QString &arg1);
+
+    void on_parametersTableWidget_itemSelectionChanged();
+
+    void on_parameterChanged(int row, int col);
 
 private:
     Ui::NodePalette *ui;
