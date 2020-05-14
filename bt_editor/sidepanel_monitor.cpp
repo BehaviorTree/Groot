870d920d2f989d6692ea4d2c3ba8372541e998f5
Sarathkrishnan Ramesh <sarathkrishnan99@gmail.com>
2020-05-15 03:00:29 +0530

    Add server and publisher port configuration (#75)
    * Add server and publisher port configuration
    
    * Remove server_port configuration
    Server and publisher ports are consecutive numbers where server port follows publisher port number.

--- bt_editor/sidepanel_monitor.cpp
+++ bt_editor/sidepanel_monitor.cpp
@@ -163,11 +163,20 @@ void SidepanelMonitor::on_Connect()
             address = ui->lineEdit->placeholderText();
             ui->lineEdit->setText(address);
         }
+
+        QString publisher_port = ui->lineEdit_publisher->text();
+        if( publisher_port.isEmpty() )
+        {
+            publisher_port = ui->lineEdit_publisher->placeholderText();
+            ui->lineEdit_publisher->setText(publisher_port);
+        }
+
         bool failed = false;
         if( !address.isEmpty() )
         {
-            _connection_address_pub = "tcp://" + address.toStdString() + std::string(":1666");
-            _connection_address_req = "tcp://" + address.toStdString() + std::string(":1667");
+            _connection_address_pub = "tcp://" + address.toStdString() + ":" + publisher_port.toStdString();
+            _connection_address_req = "tcp://" + address.toStdString() + ":" + std::to_string(publisher_port.toInt() + 1);
+
             try{
                 _zmq_subscriber.connect( _connection_address_pub.c_str() );
 
@@ -194,6 +203,7 @@ void SidepanelMonitor::on_Connect()
         {
             _connected = true;
             ui->lineEdit->setDisabled(true);
+            ui->lineEdit_publisher->setDisabled(true);
             _timer->start(20);
             connectionUpdate(true);
         }
@@ -207,6 +217,7 @@ void SidepanelMonitor::on_Connect()
     else{
         _connected = false;
         ui->lineEdit->setDisabled(false);
+        ui->lineEdit_publisher->setDisabled(false);
         _timer->stop();
 
         connectionUpdate(false);
